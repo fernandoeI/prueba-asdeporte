9b5041520d9840225afe8819eaf23b3c2d46ab42
fernandovidalturismo <fernandovidal@tabasco.gob.mx>
2025-01-14 15:27:37 -0600

    initial data fs tmp

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -11,7 +11,12 @@ export async function GET() {
   try {
     const initFile = () => {
       if (!fs.existsSync(filePath)) {
-        const initialData = { message: "Archivo creado con éxito" };
+        const initialData = [
+          {
+            id: "aeb959f1-032a-4090-bc49-87f243991348",
+            name: "sleep",
+          },
+        ];
         fs.writeFileSync(filePath, JSON.stringify(initialData, null, 2));
         console.log("Archivo creado en:", filePath);
       } else {
